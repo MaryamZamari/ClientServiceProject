f9a8713821a0c4e00870dafc95c9fffe54ecf30e
MaryamZamari <zaamari.maryam@gmail.com>
2024-01-28 07:45:27 +0100

    fixed a minor issue about declaring type

--- src/com/javase/clientservice/service/validation/IValidation.java
+++ src/com/javase/clientservice/service/validation/IValidation.java
@@ -1,7 +1,5 @@
 package com.javase.clientservice.service.validation;
 
-import com.javase.clientservice.model.Client;
-import com.javase.clientservice.model.ContactNumber;
 import com.javase.clientservice.service.exception.ValidationException;
 
 public interface IValidation<T> {
--- src/com/javase/clientservice/service/validation/NumberValidation.java
+++ src/com/javase/clientservice/service/validation/NumberValidation.java
@@ -3,7 +3,7 @@ package com.javase.clientservice.service.validation;
 import com.javase.clientservice.model.ContactNumber;
 import com.javase.clientservice.service.exception.ValidationException;
 
-public class NumberValidation implements IValidation{
+public class NumberValidation implements IValidation<ContactNumber>{
     @Override //TODO: implement it in the numberService.
     public void validate(ContactNumber contact) throws ValidationException {
         String number = contact.getNumber();
@@ -12,11 +12,9 @@ public class NumberValidation implements IValidation{
         }
         if(number.matches("^098\\d{10}$|^00\\{12}$|^+\\d{12}$")){
             throw new ValidationException("Invalid number format!");
-        } //TODO: Add regex for email and password.
-    }
+        }
+    }//TODO: Add regex for email and password.
+    //TODO: implement this class in numberService.
 
-    @Override
-    public void validate(Object o) throws ValidationException {
 
-    }
 }
